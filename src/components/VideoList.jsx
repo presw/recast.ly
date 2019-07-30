08a33a42135ac6e270534ad0dcdb3ccd8e06e312
presw <pmwhitehall@gmail.com>
2019-07-30 10:56:22 -0700

    Changed VideoListEntry from stateful to stateless component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -14,8 +14,6 @@ var VideoList = (props) => {
   );
 }
 
-
-
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
