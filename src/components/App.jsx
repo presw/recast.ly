95a735b561ee5d91458ced01089681479089d398
presw <pmwhitehall@gmail.com>
2019-07-29 21:59:38 -0700

    WIP - prop video(s); children with same key

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import VideoList from './VideoList.js'
+import VideoListEntry from './VideoListEntry.js'
+
+console.log("VideoList Import:");
+console.log(VideoList);
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +16,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em><VideoList /></h5></div>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,24 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import videoData from '../data/exampleVideoData.js'
+import VideoListEntry from './VideoListEntry.js'
+
+var myTest = videoData;
+
+console.log(videoData);
+
+var VideoList = () => {
+console.log("HERE HERE");
+console.log(videoData.snippet);
+  var vidList = videoData.map((video) =>
+    <VideoListEntry key={video}
+                    value={video} />
+  )
+
+  return (
+    <div className="video-list">{vidList}</div>
+  );
+  }
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +29,14 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+// OLD STUFF
+  // FROM VIDEOLIST
+//   {/* <div className="video-list">
+//     <div><h5><em>videoListEntry</em> view BWHAAA?A here</h5></div>
+//     <div><h5><em>videoListEntry</em> view BWHAAA?A here</h5></div>
+//     <div><h5><em>videoListEntry</em> view BWHAAA?A here</h5></div>
+//     <div><h5><em>videoListEntry</em> view BWHAAA?A here</h5></div>
+//     <div><h5><em>videoListEntry</em> view BWHAAA?A here</h5></div>
+//   </div>
+// ); */}
