baea2d9be2969be3c8b21d3bfc5024aa8ec78bcb
presw <pmwhitehall@gmail.com>
2019-07-30 19:21:32 -0700

    Implented ajax get request to YouTube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,16 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  $.ajax({
+    url: `https://www.googleapis.com/youtube/v3/search?type=video&videoEmbeddable=true`,
+    type: 'GET',
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      part: 'snippet'
+    },
+    success: (data) => callback(data.items)
+  });
 };
 
 export default searchYouTube;
